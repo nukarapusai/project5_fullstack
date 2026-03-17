efc9eec11ef08d4beb4766d769f71c7c80c98ca2
nukarapusai <nukarapusai@example.com>
2026-03-17 15:06:15 +0530

    added all stacks to CDK app

--- infra/bin/app.ts
+++ infra/bin/app.ts
@@ -1,18 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { NetworkStack } from '../lib/network-stack';
+import { BackendStack } from '../lib/backend-stack';
+import { FrontendStack } from '../lib/frontend-stack';
 
 const app = new cdk.App();
 
+// 1. Network
 const network = new NetworkStack(app, 'NetworkStack');
 
-
-import { BackendStack } from '../lib/backend-stack';
-
-const backend = new BackendStack(app, 'BackendStack', {
-  vpc: network.vpc
+// 2. Backend (ECS, ALB, RDS)
+new BackendStack(app, 'BackendStack', {
+  vpc: network.vpc,
 });
 
-
-import { FrontendStack } from '../lib/frontend-stack';
-
+// 3. Frontend (S3 + CloudFront)
 new FrontendStack(app, 'FrontendStack');
