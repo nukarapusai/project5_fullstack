78437f4ab7ac64b523821a219f90156177ded603
nukarapusai <nukarapusai@example.com>
2026-03-18 15:41:47 +0530

    fix

--- infra/bin/project5_fullstack.ts
+++ infra/bin/project5_fullstack.ts
@@ -4,11 +4,22 @@ import { BackendStack } from '../lib/backend-stack';
 import { FrontendStack } from '../lib/frontend-stack';
 
 const app = new cdk.App();
+const qualifier = process.env.CDK_QUALIFIER ?? 'p5fullstck';
+const stackDefaults: cdk.StackProps = {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+  synthesizer: new cdk.DefaultStackSynthesizer({
+    qualifier,
+  }),
+};
 
-const network = new NetworkStack(app, 'NetworkStack');
+const network = new NetworkStack(app, 'NetworkStack', stackDefaults);
 
 new BackendStack(app, 'BackendStack', {
+  ...stackDefaults,
   vpc: network.vpc,
 });
 
-new FrontendStack(app, 'FrontendStack');
+new FrontendStack(app, 'FrontendStack', stackDefaults);
